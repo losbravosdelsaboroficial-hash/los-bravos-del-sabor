37e43865c1c94e94c1752e269a60c14c3e6d3b96
losbravosdelsaboroficial-hash <losbravosdelsaboroficial@gmail.com>
2026-07-23 00:28:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,21 +25,30 @@ el.classList.add("active");
 }
   // Ocultar eventos vencidos automáticamente
 document.addEventListener("DOMContentLoaded", () => {
+
     const hoy = new Date();
     hoy.setHours(0, 0, 0, 0);
 
     document.querySelectorAll(".event-card").forEach(evento => {
-        const fechaTexto = evento.getAttribute("data-fecha");
 
-        if (!fechaTexto) return;
-
-        const fechaEvento = new Date(fechaTexto);
+        const fechaEvento = new Date(evento.dataset.fecha);
         fechaEvento.setHours(0, 0, 0, 0);
 
-        if (fechaEvento < hoy) {
-            evento.remove();
+        const diferencia = fechaEvento - hoy;
+        const dias = Math.ceil(diferencia / (1000 * 60 * 60 * 24));
+
+        const contador = evento.querySelector("#contador-evento");
+
+        if (dias > 0) {
+            contador.innerHTML = `⏳ Faltan <strong>${dias}</strong> días`;
+        } else if (dias === 0) {
+            contador.innerHTML = "🔥 <strong>¡El evento es hoy!</strong>";
+        } else {
+            contador.innerHTML = "✅ Evento realizado";
         }
+
     });
+
 });
 
 });
